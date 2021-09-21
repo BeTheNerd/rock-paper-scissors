2dc93773944bef8d5c1266db7e4b3f02881ef4e2
Kelsey Nicholes <bethenerd.dev@gmail.com>
2021-09-21 12:35:38 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import { useState } from "react";
+import "./App.css";
+import { useState, useEffect } from "react";
+import { Button } from "semantic-ui-react";
 
 
 const  App = () => {
   
   const [userChoice, setuserChoice] = useState(null)
+  const [computerChoice, setComputerChoice] = useState(null)
+  const [ result, setResult] = useState(null)
   const choices = ['rock', 'paper', 'scissors']
 
   const handleClick = (value) => {
     setuserChoice(value)
+    generateComputerChoice()
+    
   }
 
   const generateComputerChoice = () => {
-    const randomChoice = 
+    const randomChoice = choices[Math.floor(Math.random() * choices.length) ]
+    setComputerChoice(randomChoice)
+    
+  }
+
+  useEffect(() => {
+    checkResult()
+  }, [computerChoice, userChoice])
+  
+  const checkResult = () => {
+    switch( userChoice + computerChoice ) {
+      case 'scissorspaper':
+      case 'rockscissors':
+      case 'paperrock':
+        setResult("You Win!")
+        break 
+      case 'paperscissors':
+      case 'scissorsrock':
+      case 'rockpaper':
+        setResult("You Lose!")
+        break
+      case 'rockrock':
+      case 'scissorsscissors':
+      case 'paperpaper':
+        setResult('Tie!')
+        break
+
+
+    }
   }
 
   return (
     <div>
-      <h1>User Choice: {userChoice}</h1>
-      <h1>Computer Choice:</h1>
+      <h1>Rock Paper Scissors Game!</h1>
+      <h2>User Choice: {userChoice}</h2>
+      <h2>Computer Choice:{computerChoice}</h2>
       {choices.map((choice, index) => 
-        <button key={index} onClick={() => handleClick(choice)}>{choice}</button>)}
+        <Button key={index} className={choice} onClick={() => handleClick(choice)}>{choice}</Button>)}
+      <h1>{result}</h1>
     </div>
   );
 }
