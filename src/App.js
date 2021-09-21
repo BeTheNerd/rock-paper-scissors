53ec585244e2a81e95d7eca8458bfd3a1923a127
Kelsey Nicholes <bethenerd.dev@gmail.com>
2021-09-21 09:33:30 -0600

    button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+
+const  App = () => {
+  
+  const [userChoice, setuserChoice] = useState(null)
+  const choices = ['rock', 'paper', 'scissors']
+
+  const handleClick = (value) => {
+    setuserChoice(value)
+  }
+
+  const generateComputerChoice = () => {
+    const randomChoice = 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>User Choice: {userChoice}</h1>
+      <h1>Computer Choice:</h1>
+      {choices.map((choice, index) => 
+        <button key={index} onClick={() => handleClick(choice)}>{choice}</button>)}
     </div>
   );
 }
